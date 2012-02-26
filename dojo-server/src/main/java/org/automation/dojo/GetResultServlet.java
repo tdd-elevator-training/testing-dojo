002d47ef6f0fd3b033dbfb6c48767e8b7a659883
sergey <somemail@some.com>
2012-02-26 21:17:07 +0200

    Reporting servlet implementation and tests

---
+++ dojo-server/src/main/java/org/automation/dojo/GetResultServlet.java
@@ -0,0 +1,43 @@
+package org.automation.dojo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@WebServlet(name="resultServlet", urlPatterns={"/result"})
+public class GetResultServlet extends HttpServlet {
+    private ScenarioService scenarioService;
+    private Pattern pattern = Pattern.compile("scenario(\\d*)", Pattern.CASE_INSENSITIVE);
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String remoteAddr = request.getRemoteAddr();
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String parameterName = parameterNames.nextElement();
+            Matcher matcher = pattern.matcher(parameterName);
+            if (matcher.find()) {
+                boolean result = scenarioService.testResult(name, remoteAddr, Integer.parseInt(matcher.group(1)),
+                        parseResult(request.getParameter(parameterName)));
+                resp.getWriter().println(parameterName + "=" + (result ? "passed" : "failed"));
+            }
+        }
+        resp.flushBuffer();
+    }
+
+    private boolean parseResult(String value) {
+        return value.equalsIgnoreCase("passed");
+    }
+
+    public void setScenarioService(ScenarioService scenarioService) {
+        this.scenarioService = scenarioService;
+    }
+}
---
+++ dojo-server/src/main/java/org/automation/dojo/ScenarioService.java
@@ -0,0 +1,5 @@
+package org.automation.dojo;
+
+public interface ScenarioService {
+    boolean testResult(String clientName, String clientAddress, int scenarioNumber, boolean testPassed);
+}
