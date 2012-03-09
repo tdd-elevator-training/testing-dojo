d46c50cfb5913c883524cf1065f54b33d3c9c3cd
apofig <apofig@gmail.com>
2012-03-09 12:33:22 +0200

    Пофиксил айдишку сценария, а то дублировалось. Написал тест на то как выдаются айдихи у багфектори.

---
+++ dojo-server/src/test/java/org/automation/dojo/web/bugs/BugFactoryTest.java
@@ -0,0 +1,23 @@
+package org.automation.dojo.web.bugs;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class BugFactoryTest {
+    
+    @Test
+    public void shouldUniqueIds(){
+        assertEquals(1, BugsFactory.getBug(NullBug.class).getId());
+
+        assertEquals(2, BugsFactory.getBug(NoResultWhenExpectedBug.class).getId());
+        assertEquals(3, BugsFactory.getBug(AddSomeOtherElementIfListNotEmptyBug.class).getId());
+        assertEquals(4, BugsFactory.getBug(FoundNotExistsProductBug.class).getId());
+
+        assertEquals(5, BugsFactory.getBug(AddExistingItemWithPriceLessThanEntered.class).getId());
+        assertEquals(6, BugsFactory.getBug(AddExistingItemWithPriceMoreThanEntered.class).getId());
+
+        assertEquals(7, BugsFactory.getBug(BrokenSortingBug.class).getId());
+        assertEquals(8, BugsFactory.getBug(IgnorePriceSortingOrderBug.class).getId());
+    }
+}
