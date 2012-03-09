c317301b35cd070064b17cae8ae31dc222b1c0bd
unknown <serhiy.zelenin@szelenin2-pc.synapse.com>
2012-03-09 13:07:31 +0200

    Configuration service restarts schedule task with new timeout value

--- template-java/src/test/java/SampleAutomationTest.java
+++ template-java/src/test/java/SampleAutomationTest.java
@@ -4,13 +4,37 @@ import org.automation.dojo.Scenario;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.fail;
+
 @RunWith(DojoTestRunner.class)
 @ReportTo(server = "http://127.0.0.1:8080", userName = "JohnDoe")
 public class SampleAutomationTest {
 
     @Test
     @Scenario(1)
-    public void searchForProduct(){
+    public void searchForProduct() {
         //put test automation code here
+        fail();
+    }
+
+    @Test
+    @Scenario(2)
+    public void searchForProduct2() {
+        //put test automation code here
+        fail();
+    }
+
+    @Test
+    @Scenario(3)
+    public void searchForProduct3() {
+        //put test automation code here
+//        fail();
+    }
+
+    @Test
+    @Scenario(4)
+    public void searchForProduct4() {
+        //put test automation code here
+//        fail();
     }
 }
