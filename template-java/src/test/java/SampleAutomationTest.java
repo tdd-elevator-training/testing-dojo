8b176cac1904e03a3d0827075043e18813efae12
unknown <serhiy.zelenin@szelenin2-pc.synapse.com>
2012-03-10 00:43:18 +0200

    Темплейт с возможностью репортить эксепшны

--- template-java/src/test/java/SampleAutomationTest.java
+++ template-java/src/test/java/SampleAutomationTest.java
@@ -7,34 +7,12 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.fail;
 
 @RunWith(DojoTestRunner.class)
-@ReportTo(server = "http://127.0.0.1:8080", userName = "Sergey")
+@ReportTo(server = "http://127.0.0.1:8080", userName = "JohnDoe")
 public class SampleAutomationTest {
 
     @Test
     @Scenario(1)
     public void searchForProduct() {
         //put test automation code here
-//        fail();
-    }
-
-    @Test
-    @Scenario(2)
-    public void searchForProduct2() {
-        //put test automation code here
-        fail();
-    }
-
-    @Test
-    @Scenario(3)
-    public void searchForProduct3() {
-        //put test automation code here
-//        fail();
-    }
-
-    @Test
-    @Scenario(4)
-    public void searchForProduct4() {
-        //put test automation code here
-//        fail();
     }
 }
