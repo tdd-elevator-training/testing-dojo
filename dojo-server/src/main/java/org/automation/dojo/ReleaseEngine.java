0ac3660e9341cb41773ae2d8d97ca6e4a26bb2dd
apofig <apofig@gmail.com>
2012-03-09 13:22:51 +0200

    Каждый сценарий имеет свою уникальную айдиху - на них потом писать тесты бдут. Важно чтобы айдихи не пересекались. Но есть и так называемый терминаторы (Серега их назвал вьюхами), которые ничего не делают а просто перенаправляют на jsp. Я сделал так, чтобы ReleaseEngine выдавал на выход сценарии для Сереги без терминаторов потому как Серега работает с чистыми сценаряями. Айдхи у терминаторов от 1000.
    Еще я выделил метод загрузки релизов из файла в отдельный клас.

--- dojo-server/src/main/java/org/automation/dojo/ReleaseEngine.java
+++ dojo-server/src/main/java/org/automation/dojo/ReleaseEngine.java
@@ -90,52 +90,8 @@ public class ReleaseEngine {
     }
 
     public void init() {
-        try {
-            List<String> scenarioLines = IOUtils.readLines(scenarioResource.getInputStream());
-            int i = 0;
-            int currentReleaseNumber = 1;
-            Release currentRelease = new Release();
-            while (i<scenarioLines.size()) {
-                String[] scenarioParts = scenarioLines.get(i).split(",");
-                int scenarioId = Integer.parseInt(scenarioParts[0]);
-                int releaseNumber = Integer.parseInt(scenarioParts[2]);
-                String scenarioDescription = scenarioParts[1];
-                Class<BasicScenario> scenarioClass = getScenarioClassByName(scenarioParts[3]);
-
-                if (currentReleaseNumber != releaseNumber) {
-                    releases.add(currentRelease);
-                    currentReleaseNumber = releaseNumber;
-                    currentRelease = new Release();
-                }
-
-                currentRelease.addScenario(scenario(scenarioClass, scenarioId, scenarioDescription));
-                i++;
-            }
-            releases.add(currentRelease);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        releases.addAll(new ReleaseLoder(scenarioResource).getReleases(bugsQueue));
         notifyServices();
-    }
-
-    private Class<BasicScenario> getScenarioClassByName(String className) {
-        Class<?> aClass = null;
-        try {
-            aClass = this.getClass().getClassLoader().loadClass(className);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(String.format("Class %s not found in the classpath", className));
-        }
-        if (aClass != null && BasicScenario.class.isAssignableFrom(aClass)) {
-            return (Class<BasicScenario>)aClass;
-        } else {
-            throw new IllegalArgumentException("This is not scenario class: " + aClass.getName());
-        }
-    }
-
-    private BasicScenario scenario(Class<BasicScenario> scenarioClass, int id, String description) {
-        return constructor().withParameterTypes(int.class, String.class, BugsQueue.class)
-                             .in(scenarioClass)
-                             .newInstance(id, description, bugsQueue);
     }
 
     public void setScenarioResource(Resource scenarioResource) {
---
+++ dojo-server/src/main/java/org/automation/dojo/ReleaseLoder.java
@@ -0,0 +1,75 @@
+package org.automation.dojo;
+
+import org.apache.commons.io.IOUtils;
+import org.automation.dojo.web.scenario.BasicScenario;
+import org.automation.dojo.web.scenario.Release;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.fest.reflect.core.Reflection.constructor;
+
+public class ReleaseLoder {
+
+    private Resource scenarioResource;
+    private BugsQueue bugsQueue;
+
+    public ReleaseLoder(Resource scenarioResource) {
+        this.scenarioResource = scenarioResource;
+    }
+
+    public List<? extends Release> getReleases(BugsQueue bugsQueue) {
+        this.bugsQueue = bugsQueue;
+
+        List<Release> result = new LinkedList<Release>();
+        try {
+            List<String> scenarioLines = IOUtils.readLines(scenarioResource.getInputStream());
+            int i = 0;
+            int currentReleaseNumber = 1;
+            Release currentRelease = new Release();
+            while (i<scenarioLines.size()) {
+                String[] scenarioParts = scenarioLines.get(i).split(",");
+                int scenarioId = Integer.parseInt(scenarioParts[0]);
+                int releaseNumber = Integer.parseInt(scenarioParts[2]);
+                String scenarioDescription = scenarioParts[1];
+                Class<BasicScenario> scenarioClass = getScenarioClassByName(scenarioParts[3]);
+
+                if (currentReleaseNumber != releaseNumber) {
+                    result.add(currentRelease);
+                    currentReleaseNumber = releaseNumber;
+                    currentRelease = new Release();
+                }
+
+                currentRelease.addScenario(scenario(scenarioClass, scenarioId, scenarioDescription));
+                i++;
+            }
+            result.add(currentRelease);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private Class<BasicScenario> getScenarioClassByName(String className) {
+        Class<?> aClass = null;
+        try {
+            aClass = this.getClass().getClassLoader().loadClass(className);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(String.format("Class %s not found in the classpath", className));
+        }
+        if (aClass != null && BasicScenario.class.isAssignableFrom(aClass)) {
+            return (Class<BasicScenario>)aClass;
+        } else {
+            throw new IllegalArgumentException("This is not scenario class: " + aClass.getName());
+        }
+    }
+
+    private BasicScenario scenario(Class<BasicScenario> scenarioClass, int id, String description) {
+        return constructor().withParameterTypes(int.class, String.class, BugsQueue.class)
+                             .in(scenarioClass)
+                             .newInstance(id, description, bugsQueue);
+    }
+}
