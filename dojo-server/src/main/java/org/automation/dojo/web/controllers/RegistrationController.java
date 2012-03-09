466ecb3c5ff32780e5c53daa06798e4d236bc1e8
unknown <serhiy.zelenin@szelenin2-pc.synapse.com>
2012-03-09 14:15:00 +0200

    Removed java 7 classes

--- dojo-server/src/main/java/org/automation/dojo/web/controllers/RegistrationController.java
+++ dojo-server/src/main/java/org/automation/dojo/web/controllers/RegistrationController.java
@@ -6,11 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.validation.Valid;
 
 /**
  * @author serhiy.zelenin
@@ -30,7 +27,7 @@ public class RegistrationController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String submitRegistrationForm(@Valid Player player, BindingResult result) {
+    public String submitRegistrationForm(Player player, BindingResult result) {
         if (result.hasErrors()) {
             return "register";
         }
